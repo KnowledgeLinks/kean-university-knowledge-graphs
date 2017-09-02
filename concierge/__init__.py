580f1dc0cd8e3eab812faa56760e8bf7518898f9
Jeremy Nelson <jermnelson.gmail.com>
2017-09-02 14:58:17 -0600

    adding LDAP login JSON routes to app

--- concierge/__init__.py
+++ concierge/__init__.py
@@ -1,7 +1,10 @@
 __author__ = "Jeremy Nelson"
 
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
 
 from .views import *
--- concierge/views.py
+++ concierge/views.py
@@ -1,8 +1,20 @@
 __author__ = "Jeremy Nelson"
 
+from bs4 import BeautifulSoup
 from . import app
-from flask import render_template
+from flask import jsonify, render_template
+from flask import current_app, request
+from flask_ldap3_login import LDAP3LoginManager
+
+ldap_manager = LDAP3LoginManager()
+
+@app.route("/login", methods=['POST'])
+def login():
+    """Login Method """
+    username = request.form.get("username")
+    return jsonify({"username": username})
+    
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return jsonify({"about": "Kean Concierge API"})
