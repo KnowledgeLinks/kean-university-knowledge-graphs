d41cfa262152c4bdc6e6b8c31021bde6722b09ca
Jeremy Nelson <jermnelson@gmail.com>
2017-09-13 10:08:39 -0600

    added login decorator

--- concierge/views.py
+++ concierge/views.py
@@ -6,7 +6,33 @@ from flask import abort, jsonify
 from flask import current_app, request, session
 from ldap3 import Server, Connection, ALL
 
-server = Server(app.config.LDAP_HOST, get_info=ALL, use_ssl=True)
+server = Server(app.config.get('LDAP_HOST'), 
+                get_info=ALL, 
+                use_ssl=True)
+
+def login_required(f):
+    def decorator(*args, **kwargs):
+        token = kwargs.get("token")
+        if token is None:
+            abort(401)
+        user_info = jwt.decode(token, 
+                               app.config.get("SECRET_KEY"),
+                               algorithm='HS256')
+        conn = Connection(server, 
+                          app.config.get('LDAP_STUDENT_DN').format(
+                              user_info.get("username")),
+                          password=user_info.get("password"))
+        if conn.bind() is False:
+            # Tries to authenticate as staff
+            conn = Connection(server, 
+                app.config.get('LDAP_STAFF_DN').format(username),
+                password=password)
+        if conn.bind() is True:
+            return f(*args, **kwargs)
+        else:
+            return abort(401)
+    return decorator
+        
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -20,13 +46,13 @@ def login():
         error_response.status_code = 500
         return error_response
     conn = Connection(server, 
-                      app.config.LDAP_STUDENT_DN.format(username),
+                      app.config.get('LDAP_STUDENT_DN').format(username),
                       password=password)
     # Try to authenticate with credentials as a student
     if conn.bind() is False:
-        # Trys to authenticate as staff
+        # Tries to authenticate as staff
         conn = Connection(server, 
-                      app.config.LDAP_STAFF_DN.format(username),
+                      app.config.get('LDAP_STAFF_DN').format(username),
                       password=password)
     if conn.bind() is True:
         token = jwt.encode({"username": username, 
@@ -51,6 +77,7 @@ def logout():
     return jsonify({"message": "Logged out Kean Concierge API"})
 
 @app.route("/search", methods=["GET", "POST"])
+@login_required
 def catalog_search():
     """Searches Elasticsearch Index of BF 2.0 RDF for Kean"""
     token = request.form.get("token")
