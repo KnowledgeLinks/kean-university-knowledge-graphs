f8ee586b1caf3d32480e4b519bf3793867d8b891
Jeremy Nelson <jermnelson@gmail.com>
2017-09-15 16:47:38 -0600

    version bump and fixes to views

--- concierge/views.py
+++ concierge/views.py
@@ -1,6 +1,5 @@
 __author__ = "Jeremy Nelson"
 
-import logging
 import jwt
 from . import app
 from flask import abort, jsonify
@@ -11,37 +10,55 @@ server = Server(app.config.get('LDAP_HOST'),
                 get_info=ALL, 
                 use_ssl=True)
 
+def __auth__(user):
+    """Internal function takes a user dict and
+    returns a dictionary with a token or raises a 401
+    error.
+
+    Args:
+        user(dict): User creditials
+    """
+    conn = Connection(server, 
+                      app.config.get('LDAP_STUDENT_DN').format(
+                          user.get("username")),
+                      password=user.get("password"))
+    if conn.bind() is False:
+         # Tries to authenticate as staff
+         conn = Connection(server, 
+             app.config.get('LDAP_STAFF_DN').format(username),
+             password=password)
+    return conn.bind()
+
+
 def login_required(f):
     def decorator(*args, **kwargs):
         token = request.form.get('token')
-        logging.info("Token is {}".format(token))
         if token is None:
-            abort(401)
+            abort(400)
         try:
             user_info = jwt.decode(token, 
                                    app.config.get("SECRET_KEY"),
                                    algorithm='HS256')
         except jwt.exceptions.DecodeError:
-            abort(401)
-        conn = Connection(server, 
-                          app.config.get('LDAP_STUDENT_DN').format(
-                              user_info.get("username")),
-                          password=user_info.get("password"))
-        if conn.bind() is False:
-            # Tries to authenticate as staff
-            conn = Connection(server, 
-                app.config.get('LDAP_STAFF_DN').format(username),
-                password=password)
-        if conn.bind() is True:
+            abort(403)
+        is_valid = __auth__(user_info) 
+        if is_valid is True:
             return f(*args, **kwargs)
         else:
-            return abort(401)
+            return abort(403)
     return decorator
-        
+       
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify({"status": 400,
+                    "message": "Bad Request; missing parameters"}), 400
+ 
 @app.errorhandler(401)
-def bad_authentication(e):
+def unauthorized(e):
     return jsonify({"status": 401,
                     "message": "Invalid authentication"}), 401
+
+
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -54,21 +71,13 @@ def login():
              "message": "username or password cannot be blank"})
         error_response.status_code = 500
         return error_response
-    conn = Connection(server, 
-                      app.config.get('LDAP_STUDENT_DN').format(username),
-                      password=password)
-    # Try to authenticate with credentials as a student
-    if conn.bind() is False:
-        # Tries to authenticate as staff
-        conn = Connection(server, 
-                      app.config.get('LDAP_STAFF_DN').format(username),
-                      password=password)
-    if conn.bind() is True:
-        token = jwt.encode({"username": username, 
-                            "password": password},
+    user_info = {"username": username,
+                 "password": password}
+    is_valid = __auth__(user_info)
+    if is_valid is True:
+        token = jwt.encode(user_info,
                            app.config.get('SECRET_KEY'),
                            algorithm='HS256')
-        session['token'] = token
         return jsonify({"message": "Logged in".format(username),
                         "token": token.decode()})
     else:
@@ -78,22 +87,25 @@ def login():
         failed_authenticate.status_code = 403
         return failed_authenticate
 
-@app.route("/logout", methods=['GET', 'POST'])
-def logout():
-    """Logout method"""
-    if "token" in session:
-        session.pop("token")
-    return jsonify({"message": "Logged out Kean Concierge API"})
-
 @app.route("/search", methods=["GET", "POST"])
 @login_required
 def catalog_search():
     """Searches Elasticsearch Index of BF 2.0 RDF for Kean"""
-    token = request.form.get("token")
-    query = request.form.get('query')
+    if request.method.startswith("POST"):
+        token = request.form.get("token")
+        query = request.form.get('query')
+    else:
+        token = request.args.get("token")
+        query = request.args.get("query")
     search_results = []
-    return jsonify({"message": "Searched on {}".format(query),
+    return jsonify({"message": "Success",
+                    "query": query,
                     "results": search_results})
+
+@app.route("/feed", methods=["GET"])
+@login_required
+def news_feed():
+    
 
 @app.route("/")
 def home():
