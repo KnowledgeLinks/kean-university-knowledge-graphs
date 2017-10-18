8855bbd8e7e1082d0875c8ea42a30ff6a10dbcd1
Jeremy Nelson <jermnelson@gmail.com>
2017-10-18 10:53:33 -0600

    updated offset call in es_views

--- concierge/es_views.py
+++ concierge/es_views.py
@@ -117,7 +117,7 @@ def get_lookup_list(es_index, doc_type, **kwargs):
     calc = kwargs.get("calc", request.args.get("calc"))
     method = kwargs.get("method", request.args.get("method","search"))
     size = kwargs.get("size", request.args.get("size",10))
-    from_ = kwargs.get("from", request.args.get("offset"))
+    from_ = kwargs.get("offset", request.args.get("offset"))
     highlight = kwargs.get("highlight", request.args.get("highlight", False))
     search = EsBase(es_url=current_app.config.get("ELASTICSEARCH_URL"),
         es_index=es_index)
